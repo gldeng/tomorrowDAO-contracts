7286879d4bb278b1db9a63b8ccb28de81aa4b9d2
devgantmrwdao <devgan@tmrwdao.com>
2024-05-08 20:15:10 +0800

    feat: acs12

--- contracts/vote-contract/test/VoteContractTestBase.cs
+++ contracts/vote-contract/test/VoteContractTestBase.cs
@@ -8,8 +8,41 @@ namespace TomorrowDAO.Contracts.Vote;
 public class VoteContractTestBase : TestBase
 {
     protected Hash ProposalId = HashHelper.ComputeFrom("ProposalId");
-    protected Hash UniqueVoteVoteSchemeId = HashHelper.ConcatAndCompute(
-        HashHelper.ComputeFrom(Address.FromBase58("Ny5byXSUvKn2Ce7AS8CpPiCjwC3sbWPvkXMB27sXC4UG7aVHL")), 
-        HashHelper.ComputeFrom(VoteMechanism.UniqueVote.ToString()));
+    protected Hash UniqueVoteVoteSchemeId; //1a1v
+    protected Hash TokenBallotVoteSchemeId; //1t1v
     protected string TokenElf = "ELF";
+    
+    public async Task InitializeAll()
+    {
+        //init governance contrct
+        await GovernanceContractStub.Initialize.SendAsync(new Governance.InitializeInput
+        {
+            DaoContractAddress = DAOContractAddress,
+            VoteContractAddress = VoteContractAddress
+        });
+        
+        //init dao contract
+        await DAOContractStub.Initialize.SendAsync(new DAO.InitializeInput
+        {
+            GovernanceContractAddress = GovernanceContractAddress,
+            VoteContractAddress = VoteContractAddress,
+            ElectionContractAddress = ElectionContractAddress,
+            TimelockContractAddress = DefaultAddress,
+            TreasuryContractAddress = DefaultAddress
+        });
+
+        // UniqueVoteVoteSchemeId = await InitializeVoteScheme();
+        // DefaultDaoId= await InitializeDao();
+    }
+    
+    private async Task<Hash> InitializeVoteScheme(string voteMechanismString)
+    {
+        await VoteContractStub.CreateVoteScheme.SendAsync(new CreateVoteSchemeInput
+        {
+            VoteMechanism = VoteMechanism.UniqueVote
+        });
+
+        return HashHelper.ConcatAndCompute(HashHelper.ComputeFrom(VoteContractAddress),
+            HashHelper.ComputeFrom(voteMechanismString));
+    }
 }

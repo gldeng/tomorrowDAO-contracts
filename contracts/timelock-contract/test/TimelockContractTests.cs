d210d2f1201998b4d76fe68cdc72d17877565fc0
strongman6693 <strongman@tmrwdao.com>
2024-05-20 13:36:17 +0800

    delete unused UT

--- contracts/timelock-contract/test/TimelockContractTests.cs
+++ contracts/timelock-contract/test/TimelockContractTests.cs
@@ -8,20 +8,6 @@ namespace TomorrowDAO.Contracts.Timelock
     // This class is unit test class, and it inherit TestBase. Write your unit test code inside it
     public class TimelockContractTests : TestBase
     {
-        [Fact]
-        public async Task Update_ShouldUpdateMessageAndFireEvent()
-        {
-            // Arrange
-            var inputValue = "Hello, World!";
-            var input = new StringValue { Value = inputValue };
-
-            // Act
-            await TimelockContractStub.Update.SendAsync(input);
-
-            // Assert
-            var updatedMessage = await TimelockContractStub.Read.CallAsync(new Empty());
-            updatedMessage.Value.ShouldBe(inputValue);
-        }
     }
     
 }
--- contracts/treasury-contract/test/TomorrowDAO.Contracts.TreasuryTests.cs
+++ contracts/treasury-contract/test/TomorrowDAO.Contracts.TreasuryTests.cs
@@ -8,20 +8,6 @@ namespace TomorrowDAO.Contracts.Treasury
     // This class is unit test class, and it inherit TestBase. Write your unit test code inside it
     public class TreasuryContractTests : TestBase
     {
-        [Fact]
-        public async Task Update_ShouldUpdateMessageAndFireEvent()
-        {
-            // Arrange
-            var inputValue = "Hello, World!";
-            var input = new StringValue { Value = inputValue };
-
-            // Act
-            await TreasuryContractStub.Update.SendAsync(input);
-
-            // Assert
-            var updatedMessage = await TreasuryContractStub.Read.CallAsync(new Empty());
-            updatedMessage.Value.ShouldBe(inputValue);
-        }
     }
     
 }

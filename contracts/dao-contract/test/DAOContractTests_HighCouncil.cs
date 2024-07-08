f09dc70f2af7ab24537bc4c4e1924d175b21878b
strongman6693 <strongman@tmrwdao.com>
2024-07-08 21:49:26 +0800

    feat: Support for changing High Council members through proposal

--- contracts/dao-contract/test/DAOContractTests_HighCouncil.cs
+++ contracts/dao-contract/test/DAOContractTests_HighCouncil.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using AElf.Types;
 using Google.Protobuf;
 using Shouldly;
+using TomorrowDAO.Contracts.Election;
 using TomorrowDAO.Contracts.Governance;
 using Xunit;
 
@@ -157,5 +158,77 @@ public partial class DAOContractTests
 
         var result = await DAOContractStub.DisableHighCouncil.SendWithExceptionAsync(daoId);
         result.TransactionResult.Error.ShouldContain("Permission of DisableHighCouncil is not granted for");
+    }
+
+    [Fact]
+    public async Task AddHighCouncilMembersTests()
+    {
+        await InitializeAsync();
+        var daoId = await CreateDAOAsync(enableHighCouncil: true);
+        
+        var result = await CreateProposalAndVote(daoId, new ExecuteTransaction
+        {
+            ContractMethodName = nameof(DAOContractStub.AddHighCouncilMembers),
+            ToAddress = DAOContractAddress,
+            Params = new AddHighCouncilMembersInput
+            {
+                DaoId = daoId,
+                AddHighCouncils = new AddressList()
+                {
+                    Value = { UserAddress }
+                }
+            }.ToByteString()
+        });
+        var log = GetLogEvent<HighCouncilAdded>(result.TransactionResult);
+        log.DaoId.ShouldBe(daoId);
+        log.AddHighCouncils.Value.ShouldContain(UserAddress);
+    }
+    
+    [Fact]
+    public async Task RemoveHighCouncilMembersTests()
+    {
+        await InitializeAsync();
+        var daoId = await CreateDAOAsync(enableHighCouncil: true);
+        
+        var result = await CreateProposalAndVote(daoId, new ExecuteTransaction
+        {
+            ContractMethodName = nameof(DAOContractStub.AddHighCouncilMembers),
+            ToAddress = DAOContractAddress,
+            Params = new AddHighCouncilMembersInput
+            {
+                DaoId = daoId,
+                AddHighCouncils = new AddressList()
+                {
+                    Value = { UserAddress, DefaultAddress }
+                }
+            }.ToByteString()
+        });
+        var log = GetLogEvent<HighCouncilAdded>(result.TransactionResult);
+        log.DaoId.ShouldBe(daoId);
+        log.AddHighCouncils.Value.ShouldContain(UserAddress);
+        log.AddHighCouncils.Value.ShouldContain(DefaultAddress);
+        
+        result = await CreateProposalAndVote(daoId, new ExecuteTransaction
+        {
+            ContractMethodName = nameof(DAOContractStub.RemoveHighCouncilMembers),
+            ToAddress = DAOContractAddress,
+            Params = new RemoveHighCouncilInput()
+            {
+                DaoId = daoId,
+                RemoveHighCouncils = new Election.AddressList()
+                {
+                    Value = { UserAddress }
+                }
+            }.ToByteString()
+        });
+        var removeLog = GetLogEvent<HighCouncilRemoved>(result.TransactionResult);
+        removeLog.DaoId.ShouldBe(daoId);
+        removeLog.RemoveHighCouncils.Value.ShouldContain(UserAddress);
+        removeLog.RemoveHighCouncils.Value.ShouldNotContain(DefaultAddress);
+
+        var addressList = await ElectionContractStub.GetInitialHighCouncilMembers.CallAsync(daoId);
+        addressList.ShouldNotBeNull();
+        addressList.Value.ShouldContain(DefaultAddress);
+        addressList.Value.ShouldNotContain(UserAddress);
     }
 }
